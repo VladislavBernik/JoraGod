7becdf4f09068b04f6e24dafad6e1373735d7a42
”Vladislav <monsterd161@gmail.com>
2022-12-27 21:41:46 +0800

    .

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
 token = "5651587902:AAFOSYGjIZzGd3xjvy1DxVG7psoHmcevyPw"
-openai = "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"
+openai = "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"
+
+# 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
