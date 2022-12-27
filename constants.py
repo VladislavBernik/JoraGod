79738132ba0790ba0a427c969be97b29999284e9
Ibrahim <matrixo5gameplayer@gmail.com>
2022-12-27 18:25:00 +0100

    Update1

--- constants.py
+++ constants.py
@@ -1,4 +1,2 @@
 token = "5651587902:AAFOSYGjIZzGd3xjvy1DxVG7psoHmcevyPw"
-openai = "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"
-
-# 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
+openai = "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"
